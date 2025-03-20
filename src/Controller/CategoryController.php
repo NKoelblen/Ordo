1f6f62eec934c1a2858834a945c7f51061a7be1c
NKoelblen <noemie.koelblen@icloud.com>
2025-03-20 15:31:25 +0100

    WIP

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,21 +2,22 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\SpaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CategoryController extends AbstractController
 {
+    public function __construct(private SpaceRepository $spaceRepository)
+    {
+    }
+
     #[Route('/api/categories/space/{spaceId}', name: 'api_categories_by_space', methods: ['GET'])]
     public function getCategoriesBySpace(CategoryRepository $categoryRepository, int $spaceId): JsonResponse
     {
-        $categories = $categoryRepository->createQueryBuilder('c')
-            ->innerJoin('c.spaces', 's')
-            ->where('s.id = :spaceId')
-            ->setParameter('spaceId', $spaceId)
-            ->getQuery()
-            ->getResult();
+        $spaceIds = $this->spaceRepository->findAncestors($spaceId);
+        $categories = $categoryRepository->findCategoriesBySpaces($spaceIds);
 
         return $this->json(array_map(fn($category) => [
             'id' => $category->getId(),
